f8c26a7ea056561d7de4733fe0e56b0bc223b80d
Yuvraj Singh Bhadauria <114753071+yuvihere23@users.noreply.github.com>
2024-06-10 18:54:38 +0000

    updated backend url

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://mini-crm-6gp2.onrender.com',
+        target: 'https://mini-crm-6gp2.onrender.com', // Update this to your Render backend URL
+        secure: true,
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+      },
     },
   },
   plugins: [react()],
-})
+});
