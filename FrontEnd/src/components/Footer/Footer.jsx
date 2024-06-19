60c74ce6fff2e1567be2c052f238b6b93d073bdb
Yuvraj Singh Bhadauria <114753071+yuvihere23@users.noreply.github.com>
2024-06-19 16:03:24 +0000

    updated

--- FrontEnd/src/components/Footer/Footer.jsx
+++ FrontEnd/src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/yuvihere23"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
